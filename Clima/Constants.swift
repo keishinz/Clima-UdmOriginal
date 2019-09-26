9d3d79ffd81356cf58af9d90f37c87768962b2ae
kesihinz <keishin.chou@gmail.com>
2019-09-26 21:17:56 +0900

    add myAppID

---
+++ Clima/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  File.swift
+//  Clima
+//
+//  Created by Keishin CHOU on 2019/09/26.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+let myAppID = "abc"
